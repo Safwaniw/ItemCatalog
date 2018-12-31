30e6a3e26c3f0fb8c75e45e88d1a8737b40135de
Safwaniw <waleed.safwani@yahoo.com>
2018-12-31 23:43:47 +0300

    Make code PEP8 complaint

--- seeder.py
+++ seeder.py
@@ -6,59 +6,70 @@ engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
- 
+
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
- 
-user1=User(name="Waleed Al-Safwani", email ="waleed@udacity.com")
+
+user1 = User(name="Waleed Al-Safwani", email="waleed@udacity.com")
 session.add(user1)
 session.commit()
 
-user2=User(name="Ali", email ="ali@udacity.com")
+user2 = User(name="Ali", email="ali@udacity.com")
 session.add(user2)
 session.commit()
 
-category1 = Category(name="Mobile phones", description="Mobile phones and handheld device's comes under this category")
+category1 = Category(name="Mobile phones",
+                     description="Mobile phones comes under this category")
 session.add(category1)
 session.commit()
 
-item1=Item(description="IPhone 7 mobile phone", title ="iPhone 7",category=category1, user=user1)
+item1 = Item(description="IPhone 7 mobile phone",
+             title="iPhone 7", category=category1, user=user1)
 session.add(item1)
 session.commit()
 
-item2=Item(description="IPhone 8 mobile phone", title ="iPhone 8",category=category1, user=user1)
+item2 = Item(description="IPhone 8 mobile phone",
+             title="iPhone 8", category=category1, user=user1)
 session.add(item2)
 session.commit()
 
-item3=Item(description="Samsung 9s mobile phone", title ="Samsung 9s",category=category1, user=user1)
+item3 = Item(description="Samsung 9s mobile phone",
+             title="Samsung 9s", category=category1, user=user1)
 session.add(item3)
 session.commit()
 
-category2 = Category(name="Computers", description="Computer devices comes under this category")
+category2 = Category(name="Computers",
+                     description="Computer devices comes under this category")
 session.add(category2)
 session.commit()
 
-item4=Item(description="HP", title ="HP Computer",category=category2, user=user1)
+item4 = Item(description="HP",
+             title="HP Computer", category=category2, user=user1)
 session.add(item4)
 session.commit()
 
-item5=Item(description="Acer", title ="Acer Computer",category=category2, user=user1)
+item5 = Item(description="Acer",
+             title="Acer Computer", category=category2, user=user1)
 session.add(item5)
 session.commit()
 
-item6=Item(description="Dell", title ="Dell Computer",category=category2, user=user1)
+item6 = Item(description="Dell",
+             title="Dell Computer", category=category2, user=user1)
 session.add(item6)
 session.commit()
 
-category3 = Category(name="Accessories", description="Accessories comes under this category")
+category3 = Category(name="Accessories",
+                     description="Accessories comes under this category")
 session.add(category3)
 session.commit()
 
-item2=Item(description="Computer Headphone", title ="Headphone",category=category3, user=user2)
+item2 = Item(description="Computer Headphone",
+             title="Headphone", category=category3, user=user2)
 session.add(item2)
 session.commit()
 
-item2=Item(description="Computer Mouse", title ="mouse",category=category3, user=user2)
+item2 = Item(description="Computer Mouse",
+             title="mouse", category=category3, user=user2)
 session.add(item2)
 session.commit()
 
